e1acfe23205fb67438bff7768ea44726f07260c9
Ali <19alighali@georgespencer.org.uk>
2025-10-02 10:00:34 +0100

    feat: node class

---
+++ models/node.py
@@ -0,0 +1,11 @@
+from models.coordinate import CentreCoordinate, EdgeCoordinate, CornerCoordinate
+
+
+class Node:
+    def __init__(self, letter: str, coordinate: CentreCoordinate | EdgeCoordinate | CornerCoordinate):
+        self.letter = letter
+        self.selected = False
+        self.coordinate = coordinate
+
+    def __str__(self):
+        return f"{self.letter}"  # {str(self.coordinate)}
